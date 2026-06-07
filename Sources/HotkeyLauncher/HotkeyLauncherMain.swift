de85e7c7f659ff5cd8db9aa8e73d7e0b819826d5
zjy4fun <zjy4fun@gmail.com>
2026-06-07 22:23:33 +0800

    Clarify app delegate lifetime

--- Sources/HotkeyLauncher/HotkeyLauncherMain.swift
+++ Sources/HotkeyLauncher/HotkeyLauncherMain.swift
@@ -5,8 +5,8 @@ struct HotkeyLauncherMain {
     @MainActor
     static func main() {
         let app = NSApplication.shared
-        let delegate = AppDelegate()
-        app.delegate = delegate
+        let appDelegate = AppDelegate()
+        app.delegate = appDelegate
         app.run()
     }
 }
